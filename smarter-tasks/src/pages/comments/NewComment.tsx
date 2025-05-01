aa8f386630d8401f9c117c2dd2bcad26d403be63
HAMANPUREVAIBHAV <21951a05n5@iare.ac.in>
2025-05-01 23:59:16 +0530

    add comment feature

--- smarter-tasks/src/pages/comments/NewComment.tsx
+++ smarter-tasks/src/pages/comments/NewComment.tsx
@@ -69,12 +69,12 @@ const NewComment = () => {
         >
           Add Comment
         </button>
-        <div
+        <button
           onClick={closeModal}
           className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 mx-3 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
         >
           Cancel
-        </div>
+        </button>
       </form>
     </>
   );
